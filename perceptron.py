ab8386501e5e3ff868cb7d1db027bff4b8696039
koalife <flower10.star88@gmail.com>
2018-08-23 22:08:35 +0900

    initial commit

---
+++ perceptron.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class Perceptron:
+    """ AND, NAND, OR, XOR""" # comment
+
+    def AND(self,x1, x2):
+        x = np.array([x1,x2])
+        w = np.array([0.5, 0.5])
+        b = -0.7
+        tmp = np.sum(w*x) + b
+        if tmp <= 0:
+            return 0
+        else:
+            return 1
+    
+    def NAND(self,x1, x2):
+        x = np.array([x1, x2])
+        w = np.array([-0.5, -0.5])
+        b = 0.7
+        tmp = np.sum(w*x) + b
+        if tmp <= 0:
+            return 0
+        else:
+            return 1
+
+    def OR(self,x1, x2):
+        x = np.array([x1, x2])
+        w = np.array([0.5,0.5])
+        b = -0.2
+        tmp = np.sum(x*w) + b
+        if tmp <= 0:
+            return 0
+        else:
+            return 1
+    
+    def XOR(self,x1,x2):
+        s1 = self.NAND(x1,x2)
+        s2 = self.OR(x1,x2)
+        y = self.AND(s1,s2)
+        return y
+
+
+
+    
+
