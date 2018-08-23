474aa014d6b6921e0ae52021a0ed933c63815312
koalife <flower10.star88@gmail.com>
2018-08-24 08:47:26 +0900

    output_function.py

--- output_function.py
+++ output_function.py
@@ -2,7 +2,8 @@ import numpy as np
 class OutputFunction:
     """softmax_function"""
     def softmax(a):
-        exp_a = np.exp(a)
+        c = np.max(a)
+        exp_a = np.exp(a - c)     #prevent overflow
         sum_exp_a = np.sum(exp_a)
         y = exp_a / sum_exp_a
 
